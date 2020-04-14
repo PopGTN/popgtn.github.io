3a5deb6577d481f02627ce711bf613309522c829
raidcraftpvp <36484731+NormalOcean6686@users.noreply.github.com>
2020-04-14 16:33:14 -0300

    test

--- minecraft.php
+++ minecraft.php
@@ -1,23 +1,26 @@
 <?php
+
+$text = $_GET[text];
+$font = 'Mojangles.ttf';
+
+
 header("Content-Type: image/png");
-$im = @imagecreatefromjpeg('welc1.jpg')
+// $im = @imagecreatefromjpeg('image.jpg')
+$im = @imagecreate(110, 20)
     or die("Cannot Initialize new GD image stream");
-$background_color = imagecolorallocate($im, 0, 0, 0);
+
+    $background_color = imagecolorallocate($im, 0, 0, 0);
+//background color
+
 $text_color = imagecolorallocate($im, $random2, $random0, $random1);
-$black = imagecolorallocate($im, 0, 0, 0);
+//random text color
+
 $red = imagecolorallocate($im, 255, 0, 0);
 imagecolortransparent($im, $background_color);
-$font = 'verdana.ttf';
-imagestring($im, 6, 4, 10, "Name:", $text_color);
-imagestring($im, 6, 50, 10, " $motd", $text_color);
-imagestring($im, 6, 4, 30, "Software: $software", $text_color);
-imagestring($im, 6, 4, 50, "Version: $version", $text_color);
-imagestring($im, 6, 4, 50, "                 Map: $map", $text_color);
-imagestring($im, 6, 4, 70, "ServerCount: $onlineget/$onlinemax", $text_color);
-imagestring($im, 6, 170, 70, "  Class IP: $classip", $text_color);
-imagestring($im, 6, 4, 90, "Plugins: $plcount", $text_color);
-imagestring($im, 6, 170, 90, "  Class Port: $classport", $text_color);
-imagestring($im, 2, 4, 110, "Created by BlackDeadSkulll NEW UPDATE COMING SOON :)!", $black);
+imagestring($im, 2,4,110,$font, $text);
+//imagestring($im, position, "TEXT THAT YOU WANT");
 imagepng($im);
 imagedestroy($im);
+
+
 ?>
