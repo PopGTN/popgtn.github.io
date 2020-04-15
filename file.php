287ca59ab3ca326abb16beeaf9dba3c7544dee83
BlackDeadSkulll <35070173+theequaq@users.noreply.github.com>
2020-04-15 17:12:54 +0200

    Update file.php

--- file.php
+++ file.php
@@ -1,26 +1,29 @@
 <?php
-
-$text = $_GET['text'];
-//$font = 'Mojangles.ttf';
-
-
+$text = $_GET[text];
+			$random0 = rand(1, 255);
+			$random1 = rand(1, 255);
+			$random2 = rand(1, 255);
 header("Content-Type: image/png");
 $im = @imagecreatefromjpeg('image.jpg')
     or die("Cannot Initialize new GD image stream");
-
-    $background_color = imagecolorallocate($im, 0, 0, 0);
-//background color
-
+$background_color = imagecolorallocate($im, 0, 0, 0);
 $text_color = imagecolorallocate($im, $random2, $random0, $random1);
-//random text color
-
+$black = imagecolorallocate($im, 0, 0, 0);
 $red = imagecolorallocate($im, 255, 0, 0);
 imagecolortransparent($im, $background_color);
-imagestring($im, 2,4,110, $text);
-//imagestring($im, 2,4, 110, $text);
-//imagestring($im, position, "TEXT THAT YOU WANT");
+imagestring($im, 6, 4, 10, $text, $text_color);
 imagepng($im);
 imagedestroy($im);
-
-
-?>
+} else {
+header("Content-Type: image/png");
+$im = @imagecreatefromjpeg('image.jpg')
+    or die("Cannot Initialize new GD image stream");
+$background_color = imagecolorallocate($im, 255, 255, 255);
+imagecolortransparent($im, $background_color);
+$black = imagecolorallocate($im, 0, 0, 0);
+$red = imagecolorallocate($im, 255, 0, 0);
+$text_color = imagecolorallocate($im, 255, 0, 0);
+imagestring($im, 5, 10, 2,  "NOTHING ADDED", $text_color);
+imagepng($im);
+imagedestroy($im);
+}
