69ac66cef28209d2aef909d6a7544d4b4410fa83
Joshua Mckenna <36484731+PopGTN@users.noreply.github.com>
2022-11-11 20:45:05 -0400

    bmmgmlfmf
    d

---
+++ components/footer.js
@@ -0,0 +1,49 @@
+const footerTemplate = document.createElement('template');
+
+footerTemplate.innerHTML = `
+  <style>
+   @import '/Styling-Config.css';
+footer {
+    background-image: var(--Footerbodyimage) !important;
+    background-size: var(--FooterImageSize);
+    background-repeat: var(--FooterImageRepeat);
+
+    text-align: center;
+    background-color: var(--FootBackaroundColor);
+    color: var(--FootTextColour);
+    padding-top: 20px;
+    padding-bottom: 20px;
+    position: relative;
+    bottom: 0%;
+    min-width: 100vw;
+
+    /* position: fixed;
+    z-index: 1; */
+}
+footer a{
+    color: var(--FootTextColour);
+    text-decoration: none;
+    text-underline-style: none;
+}
+  </style>
+<footer class="footer" id="footer">
+    <small><a href="https://www.joshuamc.ca/" target="_blank">www.joshuamc.ca</a> Copyright © 2022</small> |
+    <small>Email:&nbsp&nbsp<a href="mailto:email@joshuamc.ca"
+                              target="_blank">email@joshuamc.ca</a></small>
+
+</footer>
+`;
+
+class Footer extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const shadowRoot = this.attachShadow({ mode: 'closed' });
+
+        shadowRoot.appendChild(footerTemplate.content);
+    }
+}
+
+customElements.define('footer-component', Footer);
---
+++ components/header.js
@@ -0,0 +1,162 @@
+class Header extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+<style>
+    @import '/Styling-Config.css';
+
+    @font-face {
+        font-family: 'Pixeltext';
+        src: url('/CSS/fonts/Pixeltext.woff2') format('woff2'),
+        url('/CSS/fonts/Pixeltext.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    }
+
+    @font-face {
+        font-family: 'ProductSans';
+        src: url('/CSS/fonts') format('woff2'),
+        url('/CSS/fonts/productsans-regular-webfont.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    }
+
+    html{
+        margin: 0px!important;
+        padding: 0px;
+    }
+    
+    body{
+        margin: 0px!important;
+        padding: 0px;
+    }
+    header{
+        block-size: border-box;
+        background-image: var(--imageNavBar);
+        background-size: var(--imageNavBarSize);
+        background-repeat: var(--imageNavBarRepeat);
+        background-color: var(--navBkGrdColour);
+        position: sticky;
+        top: 0;
+    }
+    nav {
+        display: block;
+        overflow: hidden;
+        font-family: var(--navtextfont);
+        text-align: center;
+        position: sticky;
+        color: var(--navTextColour);
+        top: 0px;
+    }
+    header {
+        padding: 0px !important;
+    }
+    
+#SiteTitle a, #SiteTitle a:hover  {
+    float: left;
+    display: block;
+    position: relative;
+    color: var(--webtitleColour);
+ 
+    margin-left: 1vw !important;
+
+    text-align: center;
+    text-decoration: none;
+    font-weight: bold;
+    font-size: 1.5em;
+    text-transform: uppercase;
+
+    border: none !important;
+    padding-left: 40px !important;
+    
+    
+    background-image: url("/photos/IMG_1768-removebg-preview (1).png") !important;
+    background-size: contain;
+    background-repeat: no-repeat;
+
+}
+
+    #navLinks {
+        float: right;
+        list-style: none;
+        display: inline-block;
+        font-family: var(--navtextfont);
+    }
+
+    #navLinks > li {
+        display: inline-flex;
+        text-align: center;
+        height: min-content;
+        padding: 0px;
+
+    }
+
+    nav > ul {
+    margin-right: 1vw;
+    padding: 0px;
+        margin-top: 0px;
+        margin-right: 1vw;
+        margin-bottom: 0px;
+
+    }
+
+    #navLinks > li > a:hover, #navLinks > li > a:active {
+
+        background-image: var(--HoverBackGroundimage);
+        background-size: var(--HoverImageSize);
+        background-color: var(--HoverColour);
+        background-repeat: var(--HoverImageRepeat);
+        font-weight: var(--HoverButtonTxtWeight);
+        padding: 0.875em 1em;
+        margin: 0px;
+        
+        border: var(--HoverButtonBorderColor) var(--HoverButtonBorderSize);
+        border-style: var(--HoverButtonBorderType);
+    }
+
+    #navLinks > li > a {
+        float: left;
+        display: block;
+        margin: 0px !important;
+        text-align: center;
+        padding: 0.875em 1em;
+        text-decoration: none;
+
+        /*Remove this When removing the border*/
+        margin-top: 11.5px;
+        margin-bottom: 4px;
+
+        color: var(--navtextColour);
+        text-decoration: none;
+        font-size: var(--navFontSize);
+        font-weight: var(--navTextWeight);
+        /*Remove this When removing the border*/
+        border: var(--ButtonBorderColor) var(--ButtonBorderSize);
+        border-style: var(--ButtonBorderType);
+
+        background-image: var(--BackGroundimage);
+        background-size: var(--ImageSize);
+        background-color: var(--BackGroundColour);
+        background-repeat: var(--NavBtnImageRepeat);
+    }
+</style>
+<header>
+    <nav>
+        <span id="SiteTitle"><a href="../index.html">Joshua Mckenna</a></span>
+        <ul id="navLinks">
+            <li><a href="/index.html">Home</a></li>
+            <li><a href="https://github.com/PopGTN" target="_blank">Projects</a></li>
+            <li><a href="/page/contact.html">Contact</a></li>
+            <li><a href="#">Updates</a></li>
+            <li><a href="/page/About-Me.html">About</a></li>
+        </ul>
+    </nav>
+</header>
+    `;
+    }
+}
+
+customElements.define('header-component', Header);
