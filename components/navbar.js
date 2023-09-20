147214cedc513c0215bdc579b1b13e99b2f587d7
Joshua Mckenna <36484731+PopGTN@users.noreply.github.com>
2023-09-20 15:10:44 -0300

    w

--- components/navbar.js
+++ components/navbar.js
@@ -28,7 +28,7 @@ class navbar extends HTMLElement {
                         test
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="https://about.joshuamc.ca">About Site</a></li>
                         <li><a class="dropdown-item" href="#">Another action</a></li>
                         <li><a class="dropdown-item" href="#">Something else here</a></li>
                     </ul>
